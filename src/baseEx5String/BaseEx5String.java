d87ac72e7c3132cb5c2d6e1f163f1bfcec36692f
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-29 10:30:20 +0100

    Question 5.5

--- src/baseEx5String/BaseEx5String.java
+++ src/baseEx5String/BaseEx5String.java
@@ -5,8 +5,23 @@ public class BaseEx5String {
     public static void main(String[] args) {
     	
         concat();
+        
         String contains = "il fait beau aujourd'hui";
         contains(contains);
+        
+        String palindrome = "Elu par cette crapule";
+        palindrome = palindrome.toLowerCase();
+        String palindrome_without_blank = removeBlank(palindrome);
+        System.out.println(palindrome_without_blank);
+        String palindrome_blank_reverse = reverse(palindrome_without_blank);
+        System.out.println(palindrome_blank_reverse);
+        
+        if(palindrome_without_blank.equals(palindrome_blank_reverse)) {
+        	System.out.println("La phrase est un palindrome");
+        }
+        else {
+        	System.out.println("La phrase n'est pas un palindrome");
+        }
     }
 
 	private static void concat() {
@@ -28,5 +43,20 @@ public class BaseEx5String {
 		else {
 			System.out.println("Erreur");
 		}
-		}
+	}
+	
+	public static String removeBlank(String palindrome) {
+	    return palindrome.replace(" ", "");
+	}
+	
+
+	public static String reverse(String texte) {
+	    String reverse = "";
+	    for (int i = texte.length() - 1; i >= 0; i--) {
+	    	reverse += texte.charAt(i);
+	    }
+	    return reverse;
+	}
+
+
 }
