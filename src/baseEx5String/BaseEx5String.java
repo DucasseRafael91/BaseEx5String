42a959f1131e487d3e7bfd8544c99896f4091e04
Rafael Ducasse <rafael.ducasse@fms-ea.com>
2025-10-29 08:48:03 +0100

    Question 5.1

--- src/baseEx5String/BaseEx5String.java
+++ src/baseEx5String/BaseEx5String.java
@@ -2,4 +2,16 @@ package baseEx5String;
 
 public class BaseEx5String {
 
+    public static void main(String[] args) {
+    	
+        String s1 = "Ceci est";
+        String s2 = "un test";
+        String resultat = s1 + " " + s2;
+        System.out.println(resultat);
+        
+        String s3 = "Ceci est";
+        String s4 = "un test 2";
+        String resultat2 = s3.concat(" ").concat(s4);
+        System.out.println(resultat2);
+    }
 }
